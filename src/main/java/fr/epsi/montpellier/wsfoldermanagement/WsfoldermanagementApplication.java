47bbd13b7c2038d348fdb5e927f704c6fdad3935
EPSI JLB <jlb.epsi@gmail.com>
2021-04-02 09:20:58 +0200

    Ajout de CORS

--- src/main/java/fr/epsi/montpellier/wsfoldermanagement/WsfoldermanagementApplication.java
+++ src/main/java/fr/epsi/montpellier/wsfoldermanagement/WsfoldermanagementApplication.java
@@ -2,6 +2,9 @@ package fr.epsi.montpellier.wsfoldermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WsfoldermanagementApplication {
@@ -9,5 +12,20 @@ public class WsfoldermanagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(WsfoldermanagementApplication.class, args);
     }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedHeaders("*")
+                        .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
+
 
 }
